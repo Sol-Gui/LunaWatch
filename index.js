7bdda1a1f306c621d8add9734cdf3df3e9d469dd
Gui.sol <int.guimarques@gmail.com>
2024-12-02 21:14:20 -0300

    minor improvements at project dir

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const path = require('path');
-const mysql = require('mysql2');
 const jwt = require('jsonwebtoken');
 const validator = require('validator');
-const bcrypt = require('bcrypt');
-const dotenv = require('dotenv');
-const { fetch_price_jupiter } = require('./public/js/prices/jupiter.cjs');
-
-const dados = require('./data.json');
+const { fetch_price_jupiter } = require('./public/js/prices/jupiter.js');
+const { connectToDatabase, createUser  } = require('./public/js/database.js');
 
 const app = express();
 const port = 80;
@@ -20,38 +16,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-dotenv.config();
-
-const db = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
-}).promise();
-
-async function connectToDatabase() {
-    try {
-        await db.connect();
-        console.log("Database conectada com sucesso!");
-    } catch (err) {
-        console.error("Erro ao conectar ao banco de dados:", err);
-    }
-}
-
 connectToDatabase();
-
-async function createUser(email, password) {
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const sql = `INSERT INTO users (email, password) VALUES (?, ?)`;
-    const [result] = await db.query(sql, [email, hashedPassword]);
-
-    console.log({
-        email: email,
-        password: hashedPassword
-    })
-
-    return result.insertId;
-}
 
 
 app.get('/', (req, res)=>{
@@ -59,31 +24,20 @@ app.get('/', (req, res)=>{
 });
 
 
-
 app.get('/login/sign-up', (req, res)=>{
     res.render('login', {method: 1});
 });
+
 
 app.get('/login/sign-in', (req, res)=>{
     res.render('login', {method: 2});
 });
 
+
 app.get('/login', (req, res)=>{
     res.render('login', {method: 1});
 });
 
-app.get('/r/:subreddit', (req, res)=>{
-
-    let subreddit = req.params.subreddit;
-    let dado = dados[subreddit];
-
-    res.render('subreddit', {subreddit, ...dado});
-});
-
-app.get('/rand', (req, res)=>{
-    let numero = Math.floor(Math.random()*10)+1;
-    res.render('rand', {numero});
-});
 
 app.post('/login/sign-in', (req, res)=>{
     const email = req.body.email;
@@ -91,6 +45,7 @@ app.post('/login/sign-in', (req, res)=>{
 
     res.send({email, password});
 });
+
 
 app.post('/login/sign-up', async (req, res)=>{
     try {
@@ -119,7 +74,6 @@ app.post('/login/sign-up', async (req, res)=>{
 });
 
 
-
 app.get('/solana/:ca', async (req, res)=>{
 
     let ca = req.params.ca;
@@ -134,25 +88,26 @@ app.get('/solana/:ca', async (req, res)=>{
     }
 });
 
+
 app.get('/tradingview', (req, res) => {
     res.render('tradingview');
 })
+
 
 app.get('/solana', (req, res) => {
     res.render('blockchains/solana');
 })
 
+
 app.get('/general', (req, res) => {
     res.render('blockchains/general');
 })
+
 
 app.get('*', (req, res)=>{
     res.render('notfound');
 });
 
-// app.use((req, res)=>{
-//     res.sendFile(path.join(__dirname, '/home.html'));
-// });
 
 app.listen(port, ()=>{
     console.log(`Ouvindo na porta ${port}!`);
